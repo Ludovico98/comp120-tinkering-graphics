adeaa3f5c5216c9e71c9fdc29665d94eef3a6655
Adrir <adrir@adrir.com>
2019-10-21 22:02:38 +0100

    Added the correct_guess and is_valid functions to __init__.py

--- guess_random/__init__.py
+++ guess_random/__init__.py
@@ -1,20 +1,58 @@
 import random
 
-randomNumber = random.randrange(0,100)
-print("Random number has been generated")
+EQUAL = 1
+LOWER = 2
+HIGHER = 3
+MAX_VALUE = 100
+MIN_VALUE = 0
+
+
+def correct_guess(target_number, user_input_number):
+    if target_number == user_input_number:
+        return EQUAL
+    elif target_number > user_input_number:
+        return HIGHER
+    elif target_number < user_input_number:
+        return LOWER
+
+
+def is_valid(expected_integer):
+    if not expected_integer.isdigit():
+        return False
+    else:
+        expected_integer = int(expected_integer)
+
+    if not expected_integer < MAX_VALUE:
+        return False
+    if not expected_integer >= MIN_VALUE:
+        return False
+
+    return True
+
+
+random_number = random.randrange(MIN_VALUE, MAX_VALUE + 1)
+print("Random number has been generated:", random_number)
 
 guessed = False
-while guessed == False:
-    userInput = int(input("Your guess pleas: "))
-    if userInput == randomNumber:
+while not guessed:
+
+    user_input = input("Your guess please: ")
+    if is_valid(user_input):
+        guess = int(user_input)
+    else:
+        print("Please use an integer between",
+              str(MIN_VALUE),
+              "and",
+              str(MAX_VALUE)
+              )
+        continue
+
+    outcome = correct_guess(random_number, guess)
+    if outcome == EQUAL:
         guessed = True
-        print("Well done!")
-    elif userInput > 100:
-        print("Our guess range is between 0 and 100, please try a bit lower")
-    elif userInput < 0:
-        print("Our guess range is between 0 and 100, please try a bit higher")
-    elif userInput > randomNumber:
-        print("Try one more time, a bit lower")
-    elif userInput < randomNumber:
-        print("Try one more time, a bit higher")
         print("You win!")
+        print("Well done!")
+    elif outcome == HIGHER:
+        print("Try one more time, a bit higher")
+    elif outcome == LOWER:
+        print("Try one more time, a bit lower")
